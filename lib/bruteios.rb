00de9701673bdf096ab7166fcbe9f3c7429b37ec
Jurvis Tan <jurvis.tan@me.com>
2017-07-04 12:44:32 +0800

    install Apt tools

--- lib/bruteios.rb
+++ lib/bruteios.rb
@@ -1,4 +1,5 @@
 require 'net/ssh'
+require 'net/scp'
 require 'table_print'
 require 'hooray'
 
@@ -6,6 +7,9 @@ require "bruteios/version"
 require "bruteios/commands_generator"
 
 module Bruteios
+  ROOT_DIR = Pathname.new(File.expand_path('../..', __FILE__))
+  ASSETS_DIR = ROOT_DIR.join("assets/")
+
   class CLILauncher
     def self.launch
       Bruteios::CommandsGenerator.start
